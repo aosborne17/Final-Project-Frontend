83aa683786dc78771a86d8628b12a15435281f8c
aosborne17 <AOsborne@spartaglobal.com>
2020-09-03 15:39:14 +0100

    first commit

---
+++ Flask/app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
+from functools import wraps
+import csv
+from boto3.session import Session
+import boto3
+import os
+
+PORT=5000
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def base():
+    ACCESS_KEY = os.environ["AWS_ACCESS_KEY"]
+    SECRET_KEY = os.environ["AWS_SECRET_KEY"]
+
+    session = Session(aws_access_key_id=ACCESS_KEY,
+                      aws_secret_access_key=SECRET_KEY)
+    s3 = session.resource('s3')
+    your_bucket = s3.Bucket('eng-67-shahrukh-devops')
+
+    for s3_file in your_bucket.objects.all():
+        print(s3_file.key)  # prints the contents of bucket
+
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+        aws_secret_access_key=os.environ["AWS_SECRET_KEY"]
+    )
+
+    s3_client.download_file('eng-67-shahrukh-devops', 'ItJobsWatchTop30Manual.csv',
+                            'Downloads/ItJobsWatchTop30Manual.csv')
+
+    return render_template("new_homepage.html")
+
+
+@app.route("/meet_team")
+def meet_team():
+    return render_template("meet_team.html")
+
+
+@app.route("/data", methods=['GET'])
+def data():
+    with open('Downloads/ItJobsWatchTop30.csv') as csv_file:
+        data = csv.reader(csv_file, delimiter=',')
+        jobs = []
+        no1job = []
+        no2job = []
+        no3job = []
+        counter = 1
+        for row in data:
+            if counter == 1:
+                no1job.append({
+                    "role": row[0],
+                    "rank": row[1],
+                    "rank_move": row[2],
+                    "median_salary": row[3],
+                    "median_percentage:": row[4],
+                    "hist_ads": row[5],
+                    "live_ads": row[6],
+                })
+                counter += 1
+                continue
+            elif counter == 2:
+                no2job.append({
+                    "role": row[0],
+                    "rank": row[1],
+                    "rank_move": row[2],
+                    "median_salary": row[3],
+                    "median_percentage:": row[4],
+                    "hist_ads": row[5],
+                    "live_ads": row[6],
+                })
+                counter += 1
+                continue
+            elif counter == 3:
+                no3job.append({
+                    "role": row[0],
+                    "rank": row[1],
+                    "rank_move": row[2],
+                    "median_salary": row[3],
+                    "median_percentage:": row[4],
+                    "hist_ads": row[5],
+                    "live_ads": row[6],
+                })
+                counter += 1
+                continue
+
+            jobs.append({
+                "role": row[0],
+                "rank": row[1],
+                "rank_move": row[2],
+                "median_salary": row[3],
+                "median_percentage:": row[4],
+                "hist_ads": row[5],
+                "live_ads": row[6],
+            })
+
+    return render_template("datapage.html",
+                           jobs=jobs, jobs1=no1job, jobs2=no2job, jobs3=no3job)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly (so this will only respond to the 404 error)
+    return render_template("404page.html"), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=PORT)
